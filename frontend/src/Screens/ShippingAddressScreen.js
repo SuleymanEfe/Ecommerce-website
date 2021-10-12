b5c20a70c9126769554d6a9462a454e15b11c1e2
Süleyman efe Çelik <suleymanefeclk2@gmail.com>
2021-10-12 12:56:01 -0700

    Shipping address reducer added and auto fill on shipping page

--- frontend/src/Screens/ShippingAddressScreen.js
+++ frontend/src/Screens/ShippingAddressScreen.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { saveShippingAddress } from "../actions/cartActions";
 import CheckoutSteps from "../components/CheckoutSteps";
 
-export default function ShippingAddressScreen() {
-  const [fullName, setFullName] = useState("");
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setcountry] = useState("");
+export default function ShippingAddressScreen(props) {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  if (!userInfo) {
+    props.history.push("/signin");
+  }
+
+  const [fullName, setFullName] = useState(shippingAddress.fullName);
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+
+  const dispatch = useDispatch();
 
   const submitHandler = (e) => {
-    e.prevent.default();
-    //TODO: dispatch save shipping address action
+    e.preventDefault();
+    dispatch(
+      saveShippingAddress({ fullName, address, city, postalCode, country })
+    );
+    props.history.push("/payment");
   };
+
   return (
     <div>
       <CheckoutSteps step1 step2></CheckoutSteps>
@@ -19,67 +37,61 @@ export default function ShippingAddressScreen() {
         <div>
           <h1>Shipping Address</h1>
         </div>
-
         <div>
           <label htmlFor="fullName">Full Name</label>
           <input
             type="text"
             id="fullName"
-            placeholder="Enter Full Name"
+            placeholder="Enter full name"
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
             required
           ></input>
         </div>
-
         <div>
           <label htmlFor="address">Address</label>
           <input
             type="text"
             id="address"
-            placeholder="Enter Address"
+            placeholder="Enter address"
             value={address}
             onChange={(e) => setAddress(e.target.value)}
             required
           ></input>
         </div>
-
         <div>
           <label htmlFor="city">City</label>
           <input
             type="text"
             id="city"
-            placeholder="Enter City"
+            placeholder="Enter city"
             value={city}
             onChange={(e) => setCity(e.target.value)}
             required
           ></input>
         </div>
-
         <div>
           <label htmlFor="postalCode">Postal Code</label>
           <input
             type="text"
             id="postalCode"
-            placeholder="Enter Postal Code"
+            placeholder="Enter postal code"
             value={postalCode}
             onChange={(e) => setPostalCode(e.target.value)}
             required
           ></input>
         </div>
-
         <div>
           <label htmlFor="country">Country</label>
           <input
             type="text"
             id="country"
-            placeholder="Enter Country"
+            placeholder="Enter country"
             value={country}
-            onChange={(e) => setcountry(e.target.value)}
+            onChange={(e) => setCountry(e.target.value)}
             required
           ></input>
         </div>
-
         <div>
           <label />
           <button className="primary" type="submit">
