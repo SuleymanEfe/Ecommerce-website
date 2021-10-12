a4b50ac6cc878413553f12226196f86866272fb1
Süleyman efe Çelik <suleymanefeclk2@gmail.com>
2021-10-11 21:58:22 -0700

    added error message to sign-in page

--- frontend/src/Screens/SignInScreen.js
+++ frontend/src/Screens/SignInScreen.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { signin } from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 export default function SignInScreen(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
+  const { userInfo, loading, error } = userSignin;
 
   const redirect = props.location.search
     ? props.location.search.split("=")[1]
@@ -33,6 +35,8 @@ export default function SignInScreen(props) {
         <div>
           <h1>Sign-in</h1>
         </div>
+        {loading && <LoadingBox />}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email address</label>
           <input
