f87dbf41901c3780bf3101542dd06c5f567ddd3b
Süleyman efe Çelik <suleymanefeclk2@gmail.com>
2021-10-12 10:37:32 -0700

    Shipping address screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import CartScreen from "./Screens/CartScreen";
 import SignInScreen from "./Screens/SignInScreen";
 import { signout } from "./actions/userActions";
 import RegisterScreen from "./Screens/RegisterScreen";
+import ShippingAddressScreen from "./Screens/ShippingAddressScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -59,6 +60,7 @@ function App() {
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/signin" component={SignInScreen} />
           <Route path="/register" component={RegisterScreen} />
+          <Route path="/shipping" component={ShippingAddressScreen} />
           <Route exact path="/" component={HomeScreen} />
         </main>
         <footer className="row center">All right reserved</footer>
