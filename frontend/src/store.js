41fb0b603b4a86bf92c52f4836f1feb096f9718a
Süleyman efe Çelik <suleymanefeclk2@gmail.com>
2021-10-11 18:46:39 -0700

    User signin reducer created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,11 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
+import { userSigninReducer } from "./reducers/userReducers";
 import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
-
 const initialState = {
   cart: {
     cartItems: localStorage.getItem("cartItems")
@@ -17,6 +17,7 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
